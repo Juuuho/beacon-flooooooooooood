7f0cee24fab224f5a0a25448a03cf188fa1302f2
Juuuho <gjwngh02@gmail.com>
2023-01-16 13:51:06 +0900

    update beacon-flood ssid-list

--- beacon-flood.py
+++ beacon-flood.py
@@ -5,13 +5,15 @@ import random
 import threading
 
 def send_beacon(netSSID, mac, infinite=True):
-    ''' 왜 안될까요..ㅠㅠ
+    '''
     radiotap = RadioTap(present=0xa00048ee, Flags=0x10, Rate=1, ChannelFrequency=2412,ChannelFlags=0x00a0, dBm_AntSignal=-95, dBm_AntNoise=-1, Lock_Quality=100)
     '''
+
     dot11 = Dot11(type=0, subtype=8, addr1 = 'ff:ff:ff:ff:ff:ff', addr2 = mac, addr3 = mac)
     beacon = Dot11Beacon(cap='ESS+privacy')
     essid = Dot11Elt(ID='SSID', info=netSSID, len=len(netSSID))
-    
+
+    '''
     rsn = Dot11Elt(ID='RSNinfo', info=(
     '\x01\x00'
     '\x00\x0f\xac\x02'
@@ -21,8 +23,8 @@ def send_beacon(netSSID, mac, infinite=True):
     '\x01\x00'
     '\x00\x0f\xac\x02'
     '\x00\x00'))
-    
-    frame = RadioTap()/dot11/beacon/essid/rsn/Dot11EltRates()
+    '''
+    frame = RadioTap()/dot11/beacon/essid
     
     '''
     frame.show()
@@ -42,7 +44,7 @@ if __name__ == '__main__':
     print(f'Argument : {argument}')
 
     if len(argument) != 2:
-        print("syntax : python bf.py <interface> <ssid-list-file>\nsample : python bf.py mon0 ssid-list.txt")
+        print("syntax : python beacon-flood.py <interface> <ssid-list-file>\nsample : python beacon-flood.py mon0 ssid-list.txt")
         quit()
     
     filename = argument[1]
